0051925f31750f1fa00e0e1d00d18784a9dcbf6a
Andrey Babichev <Al.94_94@mail.ru>
2021-02-28 23:41:00 +0300

    JRAB-1 Add TelegramBot to the project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JavaRushArticlesBot
+
+#0.1.0-SNAPSHOT:
+JRAB-0, JRAB-1. Add SpringBoot skeleton and TelegramBot to the app.
---
+++ src/main/java/com/warder/jrtb/bot/JRTelegramBot.java
@@ -0,0 +1,45 @@
+package com.warder.jrtb.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class JRTelegramBot extends TelegramLongPollingBot {
+    @Value(value = "${bot.name}")
+    private String name;
+
+    @Value(value = "${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage sm = new SendMessage();
+            sm.setChatId(chatId);
+            sm.setText(message);
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
